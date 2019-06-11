67af89c3547cd357b658d23dd7082bba8db4fc8c
SG24 <shreyanshgupta98@gmail.com>
2019-06-11 16:17:08 +0530

    added login form

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,7 @@
 import React from "react";
 import "bulma/css/bulma.min.css";
 import "./App.css";
+import Header from "./Header";
 
 // Declaring Components
 class Login extends React.Component {
@@ -14,7 +15,36 @@ class Login extends React.Component {
 
   render() {
     return (
-      <h1>Login</h1>
+      <div>
+
+      <Header isOpen={this.props.isOpen} title="Login"/>
+
+      <form action="" method="POST" className="container">
+
+        <div className="field">
+          <label className="label">Username</label>
+          <div className="control has-icons-left has-icons-right">
+            <input className="input" type="text" placeholder="Username" />
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input className="input" type="password" placeholder="**********" />
+          </div>
+        </div>
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link">Login</button>
+          </div>
+          <div className="control">
+            <button className="button is-text">Cancel</button>
+          </div>
+        </div>
+      </form>
+    </div>
     );
   }
 }
