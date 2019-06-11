33187dd669128a7de7a2f7c2702bb99cc8aa229b
SG24 <shreyanshgupta98@gmail.com>
2019-06-11 13:05:13 +0530

    added not found page

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,6 +2,7 @@
 import React from "react";
 import "bulma/css/bulma.min.css";
 import "./App.css";
+import {Link} from "react-router-dom";
 
 // Declaring Components
 class NotFound extends React.Component {
@@ -14,7 +15,10 @@ class NotFound extends React.Component {
 
   render() {
     return (
-      <h1>Page Not Found</h1>
+      <div className="grid-center margin-top-50px container">
+        <h1 className="title is-1">404 Page Not Found</h1>
+        <Link to="/" className="button is-success">Go to Home</Link>
+      </div>
     );
   }
 }
