6e1a8758ea82027b09ded6d675ae2749995393e9
SG24 <shreyanshgupta98@gmail.com>
2019-06-12 15:10:40 +0530

    uses axios to send post requests for login and signup

--- src/Login.js
+++ src/Login.js
@@ -4,47 +4,81 @@ import "bulma/css/bulma.min.css";
 import "./App.css";
 import Header from "./Header";
 
+import axios from "axios";
+
+// declaring constants
+const LOGIN_URL = "/users/login";
+
 // Declaring Components
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      username: "",
+      password: "",
     };
   }
 
+  // handling input on change event
+  handleInputChange = (event) => {
+    // updating state
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  // handling submit
+  handleSubmit = (event) => {
+    // preventing the page from loading
+    event.preventDefault();
+    // extracting user info and resetting state
+    let { username, password } = this.state;
+    this.setState({
+      username: "",
+      password: "",
+    });
+    // sending user info to server and storing the received token if success, redirecting to home
+    axios.post(LOGIN_URL, {username, password})
+      .then(data => {
+        if(data.data.success) this.props.history.push("/");
+        else alert(data.data.err);
+      })
+      .catch(e => console.log(e));
+  }
+
   render() {
+    // extracting value from state
+    let { username, password } = this.state;
+
     return (
       <div>
 
-      <Header isOpen={this.props.isOpen} title="Login"/>
+        <Header title="Login" />
 
-      <form action="" method="POST" className="container">
+        <form className="container">
 
-        <div className="field">
-          <label className="label">Username</label>
-          <div className="control has-icons-left has-icons-right">
-            <input className="input" type="text" placeholder="Username" />
+          <div className="field">
+            <label className="label">Username</label>
+            <div className="control has-icons-left has-icons-right">
+              <input onChange={(event) => this.handleInputChange(event)} name="username" value={username} className="input" type="text" placeholder="Username" />
+            </div>
           </div>
-        </div>
 
-        <div className="field">
-          <label className="label">Password</label>
-          <div className="control">
-            <input className="input" type="password" placeholder="**********" />
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control">
+              <input onChange={(event) => this.handleInputChange(event)} name="password" value={password} className="input" type="password" placeholder="**********" />
+            </div>
           </div>
-        </div>
 
-        <div className="field is-grouped">
-          <div className="control">
-            <button className="button is-link">Login</button>
+          <div className="field is-grouped">
+            <div className="control">
+              <button onClick={(event) => this.handleSubmit(event)} className="button is-link">Login</button>
+            </div>
+            <div className="control">
+              <button type="reset" className="button is-text">Cancel</button>
+            </div>
           </div>
-          <div className="control">
-            <button className="button is-text">Cancel</button>
-          </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
     );
   }
 }
--- src/SignUp.js
+++ src/SignUp.js
@@ -2,52 +2,87 @@
 import React from "react";
 import "bulma/css/bulma.min.css";
 import "./App.css";
+import axios from "axios";
 
 // Importing Components
 import Header from "./Header";
+
+// Declaring Constants
+let SIGNUP_URL = "/users/signup";
 
 // Declaring Components
 class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      username: "",
+      password: "",
+      email: "",
     };
   }
 
+  // handling input change event
+  handleInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  // handling submit
+  handleSubmit = (event) => {
+    // prevent the page from reloading 
+    event.preventDefault();
+    // extracting user info and resetting state
+    let { username, password, email } = this.state;
+    this.setState({
+      username: "",
+      password: "",
+      email: "",
+    });
+    // sending user info to server and redirecting to login if success
+    axios.post(SIGNUP_URL, { username, password, email })
+      .then(data => {
+        if(data.data.success) this.props.history.push("/login");
+        else alert(data.data.err);
+      })
+      .catch(e => console.log(e));
+  }
+
   render() {
+    // Extracting info from state
+    let { username, password, email } = this.state;
+
+    // Returning JSx
     return (
 
       <div>
 
-        <Header isOpen={this.props.isOpen} title="SignUp"/>
+        <Header title="SignUp" />
 
-        <form action="" method="POST" className="container">
+        <form className="container">
 
           <div className="field">
             <label className="label">Username</label>
             <div className="control has-icons-left has-icons-right">
-              <input className="input" type="text" placeholder="Username" />
+              <input value={username} onChange={(event) => this.handleInputChange(event)} className="input" name="username" type="text" placeholder="Username" />
             </div>
           </div>
 
           <div className="field">
             <label className="label">Email</label>
             <div className="control has-icons-left has-icons-right">
-              <input className="input" type="email" placeholder="Email" />
+              <input value={email} onChange={(event) => this.handleInputChange(event)} className="input" name="email" type="email" placeholder="Email" />
             </div>
           </div>
 
           <div className="field">
             <label className="label">Password</label>
             <div className="control">
-              <input className="input" type="password" placeholder="**********" />
+              <input value={password} onChange={(event) => this.handleInputChange(event)} className="input" name="password" type="password" placeholder="**********" />
             </div>
           </div>
 
           <div className="field is-grouped">
             <div className="control">
-              <button className="button is-link">SignUp</button>
+              <button onClick={this.handleSubmit} className="button is-link">SignUp</button>
             </div>
             <div className="control">
               <button type="reset" className="button is-text">Cancel</button>
